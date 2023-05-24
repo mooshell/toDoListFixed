70485580c3b9f80d32e8d3c4dabd2fa7879a4190
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-05-24 12:38:06 -0400

    Iteration 1 completed

--- toDoListFixed/ContentView.swift
+++ toDoListFixed/ContentView.swift
@@ -8,21 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var toDoItems: [ToDoItem] = []
+    @State private var showNewTask = false
     var body: some View {
         VStack {
             Text("To Do List")
                  .font(.system(size: 40))
                  .fontWeight(.black)
-            Spacer()
             Button(action: {
-                                
+                self.showNewTask = true
             }) {
             Text("+")
             }
             HStack {
             }
             .padding()
+        List {
+                ForEach (toDoItems) { toDoItem in
+                    if toDoItem.isImportant == true {
+                        Text("‼️" + toDoItem.title)
+                    } else {
+                        Text(toDoItem.title)
+                    }
+                        }
+            }
+        .listStyle(.plain)
         }
+        if showNewTask {
+            NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+                }
+        
     }
 }
 
