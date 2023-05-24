ecd3104c11fc3922c1533cedb8ee8bd83c1ff878
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-05-23 23:06:16 -0400

    Initial Commit

---
+++ toDoListFixed/toDoListFixedApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListFixedApp.swift
+//  toDoListFixed
+//
+//  Created by Michelle Elias Flores on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListFixedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
